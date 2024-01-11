1903b9f66434e6e6cb1bba6c335ca2f2350c910b
AgustinMadygraf <agustin.mtto.madygraf>
2024-01-11 10:29:57 -0300

    a

--- manipulacion_archivos.py
+++ manipulacion_archivos.py
@@ -1,60 +1,53 @@
 import os
 import logging
 
+logging.basicConfig(filename='logs/manipulacion_archivos.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def filtrar_archivos_por_extension(archivos, extensiones):
     """
     Filtra una lista de archivos, retornando aquellos que coinciden con las extensiones dadas.
 
-    Parámetros:
-    archivos (list of str): Lista de nombres de archivos a filtrar.
-    extensiones (list of str): Extensiones para usar en el filtrado.
+    Args:
+        archivos (list of str): Lista de nombres de archivos a filtrar.
+        extensiones (list of str): Extensiones para usar en el filtrado.
 
-    Retorna:
-    list of str: Lista de archivos filtrados que coinciden con las extensiones.
+    Returns:
+        list of str: Lista de archivos filtrados que coinciden con las extensiones.
     """
-    archivos_filtrados = []
-    for archivo in archivos:
-        if any(archivo.endswith(ext) for ext in extensiones):
-            archivos_filtrados.append(archivo)
-    return archivos_filtrados
+    return [archivo for archivo in archivos if any(archivo.endswith(ext) for ext in extensiones)]
 
-def listar_archivos(ruta, extensiones):
-    try:
-        archivos_json, archivos_sql, otros_archivos = [], [], []
-        estructura = []
+def listar_archivos(ruta, extensiones=None):
+    """
+    Lista los archivos en una ruta dada, opcionalmente filtrando por extensiones.
 
-        for raiz, _, archivos in os.walk(ruta):
-            if '.git' in raiz:
-                continue
+    Args:
+        ruta (str): Ruta del directorio a explorar.
+        extensiones (list of str, optional): Extensiones para filtrar archivos. Si es None, lista todos los archivos.
 
-            nivel = raiz.replace(ruta, '').count(os.sep)
-            indentacion = ' ' * 4 * nivel
-            estructura.append(f"{indentacion}{os.path.basename(raiz)}/")
-            subindentacion = ' ' * 4 * (nivel + 1)
+    Returns:
+        list of str: Lista de archivos encontrados.
+        list of str: Estructura de directorio y archivos.
+    """
+    archivos_encontrados = []
+    estructura = []
 
-            archivos_en_raiz = [os.path.join(raiz, archivo) for archivo in archivos]
-            archivos_filtrados = filtrar_archivos_por_extension(archivos_en_raiz, extensiones)
+    for raiz, _, archivos in os.walk(ruta):
+        if '.git' in raiz:  # Ignorar directorios .git
+            continue
 
-            for archivo in archivos_filtrados:
-                estructura.append(f"{subindentacion}{os.path.basename(archivo)}")
-                if archivo.endswith('.json'):
-                    archivos_json.append(archivo)
-                elif archivo.endswith('.sql'):
-                    archivos_sql.append(archivo)
-                else:
-                    otros_archivos.append(archivo)
+        nivel = raiz.replace(ruta, '').count(os.sep)
+        indentacion = ' ' * 4 * nivel
+        estructura.append(f"{indentacion}{os.path.basename(raiz)}/")
+        subindentacion = ' ' * 4 * (nivel + 1)
 
-        archivos_encontrados = otros_archivos + archivos_sql + archivos_json
-        return archivos_encontrados, estructura
-    except Exception as e:
-        logging.error(f"Error al listar archivos en {ruta}: {e}")
-        return [], []
-    
-def escribir_contenido_archivo(archivo, archivo_txt):
-    try:
-        with open(archivo, 'r', encoding='utf-8') as file:
-            archivo_txt.write("\n\n```\n\n")
-            archivo_txt.write(f"# {archivo}\n")
-            archivo_txt.writelines(linea for linea in file if not linea.strip().startswith('#'))
-    except Exception as e:
-        logging.error(f"Error al escribir el contenido del archivo {archivo}: {e}")
+        archivos_en_raiz = [os.path.join(raiz, archivo) for archivo in archivos]
+        archivos_filtrados = archivos_en_raiz if extensiones is None else filtrar_archivos_por_extension(archivos_en_raiz, extensiones)
+        estructura.extend(f"{subindentacion}{os.path.basename(archivo)}" for archivo in archivos_filtrados)
+        archivos_encontrados.extend(archivos_filtrados)
+
+    return archivos_encontrados, estructura
+
+# Ejemplo de uso
+# ruta = 'ruta/a/tu/directorio'
+# extensiones = ['.txt', '.py']
+# archivos, estructura = listar_archivos(ruta, extensiones)
