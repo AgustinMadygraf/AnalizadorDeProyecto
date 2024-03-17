7e2fa7d2a0be74cf1ac91c34c91768f31ea72e3d
AgustinMadygraf <agustin.mtto.madygraf>
2024-03-17 11:56:48 -0300

    Update import statement for output generation module

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # 
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src"))  # Directorio src
 
 from file_operations import listar_archivos
-from data_output import generar_archivo_salida
+from src.output_generation import generar_archivo_salida
 from utilities import obtener_version_python, limpieza_pantalla
 from user_interface import  menu_0,menu_1, menu_2,menu_3 #, menu_4
 from logs.config_logger import configurar_logging
