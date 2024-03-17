6ffbd5d3ed01d7dc33a90b447c925e8fbfe65ea3
AgustinMadygraf <agustin.mtto.madygraf>
2024-03-17 03:07:48 -0300

    Refactor logging configuration

--- src/logs/config_logger.py
+++ src/logs/config_logger.py
@@ -1,44 +1,29 @@
 #src/logs/config_logger.py
-import logging
-from logging.handlers import RotatingFileHandler
-import datetime
+
+import logging.config
 import os
+import json
+
+def configurar_logging(default_path='config/logging.json', default_level=logging.INFO, env_key='LOG_CFG'):
+    """Configura el logging basado en un archivo JSON."""
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+    
+    return logging.getLogger(__name__)
 
 class InfoErrorFilter(logging.Filter):
     def filter(self, record):
         # Permitir solo registros de nivel INFO y ERROR
         return record.levelno in (logging.INFO, logging.ERROR)
 
-def configurar_logging(nivel=logging.INFO):
-    logger = logging.getLogger()
-    if logger.hasHandlers():
-        return logger
-
-    # Configuración básica
-    filename = 'src/logs/sistema.log'
-    format = '%(asctime)s - %(levelname)s - %(module)s - %(filename)s:%(lineno)d: %(message)s'
-    maxBytes = 10485760  # 10MB
-    backupCount = 5
-    formatter = logging.Formatter(format)
-
-    # File Handler
-    file_handler = RotatingFileHandler(filename, maxBytes=maxBytes, backupCount=backupCount)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-
-    # Console Handler con filtro personalizado
-    console_handler = logging.StreamHandler()
-    console_handler.addFilter(InfoErrorFilter())  # Aplicar el filtro
-    console_handler.setFormatter(formatter)
-
-    logger.setLevel(nivel)
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-
-    logger.info("\n\n--------------- Nueva Sesión - {} - Nivel de Registro: {} ---------------\n\n".format(
-        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), logging.getLevelName(logger.getEffectiveLevel())))
-
-    return logger
 
 # Configurar el logger con un nivel específico
-configurar_logging(nivel=logging.DEBUG)
+configurar_logging()
