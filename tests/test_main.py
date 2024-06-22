9764142239b72caf852525efc43734d967fff001
AgustinMadygraf <agustin.mtto.madygraf>
2024-06-22 11:22:59 -0300

    Refactor file paths to use lowercase 'docs' directory instead of 'DOCS'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,15 @@
-#AnalizadorDeProyecto\tests\test_main.py
+#AnalizadorDeProyectos/tests/test_main.py
 import sys
 import os
+import subprocess
 
-# Agregar el directorio raíz del proyecto a sys.path
-project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(project_path)
+# Asegúrate de que el directorio raíz esté en el PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import setup
 import pytest
+from setup import check_dependencies
+from src.installer_utils import crear_archivo_bat_con_pipenv
 
-
-
-# Fixture para preparar el entorno de pruebas
 @pytest.fixture
 def directorio_temporal(tmpdir):
     # Creamos un directorio temporal para las pruebas
@@ -23,16 +21,18 @@ def directorio_temporal(tmpdir):
 def test_generate_bat_file_with_pipenv(directorio_temporal):
     # Creamos un archivo BAT en el directorio temporal
     ruta_archivo_bat = directorio_temporal.join("test.bat")
-    python_executable = "/usr/bin/python3"  # Ruta al ejecutable de Python
-    setup.generate_bat_file_with_pipenv(str(directorio_temporal), python_executable)
+    python_executable = sys.executable  # Ruta al ejecutable de Python
+
+    # Llamamos a la función para crear el archivo BAT
+    crear_archivo_bat_con_pipenv(directorio_temporal, "test_project")
 
     # Verificamos que el archivo BAT se haya creado correctamente
     assert ruta_archivo_bat.exists()
 
 def test_limpieza_pantalla(capfd):
     # Simulamos la limpieza de la pantalla
-    setup.limpieza_pantalla()
+    check_dependencies()
 
     # Capturamos la salida de la función y verificamos que se haya limpiado la pantalla
     out, _ = capfd.readouterr()
-    assert "Pantalla limpiada." in out
+    assert "Todas las dependencias están instaladas." in out
