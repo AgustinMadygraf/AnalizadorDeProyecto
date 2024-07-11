538ecd1bbdaea4618b226a09d932516651e24ed6
AgustinMadygraf <agustin.mtto.madygraf>
2024-07-11 08:33:56 -0300

    Refactor file paths to use lowercase 'docs' directory instead of 'DOCS'

--- src/models/update_repo.py
+++ src/models/update_repo.py
@@ -27,6 +27,32 @@ class RepoUpdater:
         logger.addHandler(handler)
         return logger
 
+    def git_status(self):
+        """
+        Verifica el estado del repositorio.
+        """
+        try:
+            # Ejecuta el comando git status
+            result = subprocess.run(['git', 'status'], capture_output=True, text=True)
+            self.logger.debug(f"Salida de git status: {result.stdout}")
+            if result.stderr:
+                self.logger.error(f"Error en git status: {result.stderr}")
+        except Exception as e:
+            self.logger.error(f"Error al realizar git status: {e}")
+
+    def git_restore(self):
+        """
+        Restaura los cambios locales no deseados.
+        """
+        try:
+            # Ejecuta el comando git restore para descartar cambios locales
+            result = subprocess.run(['git', 'restore', '.'], capture_output=True, text=True)
+            self.logger.debug(f"Salida de git restore: {result.stdout}")
+            if result.stderr:
+                self.logger.error(f"Error en git restore: {result.stderr}")
+        except Exception as e:
+            self.logger.error(f"Error al realizar git restore: {e}")
+
     def git_pull(self):
         """
         Realiza un git pull en el repositorio especificado en repo_path.
@@ -51,9 +77,12 @@ class RepoUpdater:
         Ejecuta el proceso de actualización del repositorio.
         """
         self.logger.debug("Iniciando el proceso de git pull")
+        self.git_status()
+        self.git_restore()
         self.git_pull()
         
 if __name__ == "__main__":
-    repo_path = 'C:\AppServ\www\AnalizadorDeProyecto'
+    # Ruta local al directorio del repositorio clonado
+    repo_path = 'C:\\AppServ\\www\\AnalizadorDeProyecto'  # Cambia esto a la ruta de tu repositorio
     updater = RepoUpdater(repo_path)
     updater.run()
