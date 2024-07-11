79dc5aa58315d4ba062fcb0c3c3cd1b64ee01d63
AgustinMadygraf <agustin.mtto.madygraf>
2024-07-11 08:28:49 -0300

    Refactor file paths to use lowercase 'docs' directory instead of 'DOCS'

--- run.py
+++ run.py
@@ -6,6 +6,11 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
 from src.app import run_app
+from src.models.update_repo import RepoUpdater
 
 if __name__ == '__main__':
+    repo_path = 'https://github.com/AgustinMadygraf/analizadorDeProyecto/'
+    updater = RepoUpdater(repo_path)
+    updater.run()
+    input("Presiona Enter para continuar...")
     run_app()
---
+++ src/models/update_repo.py
@@ -0,0 +1,59 @@
+#AnalizadorDeProyecto/src/models/update_repo.py
+import os
+import subprocess
+import logging
+
+class RepoUpdater:
+    def __init__(self, repo_path):
+        """
+        Inicializa la clase RepoUpdater con la ruta del repositorio.
+
+        :param repo_path: Ruta del repositorio.
+        """
+        self.repo_path = repo_path
+        self.logger = self.setup_logger()
+
+    def setup_logger(self):
+        """
+        Configura el logger para la clase RepoUpdater.
+
+        :return: Objeto logger configurado.
+        """
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        return logger
+
+    def git_pull(self):
+        """
+        Realiza un git pull en el repositorio especificado en repo_path.
+        """
+        try:
+            # Cambia al directorio del repositorio
+            os.chdir(self.repo_path)
+            self.logger.debug(f"Directorio cambiado a {self.repo_path}")
+
+            # Ejecuta el comando git pull
+            result = subprocess.run(['git', 'pull'], capture_output=True, text=True)
+            
+            # Imprime la salida del comando git pull
+            self.logger.debug(f"Salida de git pull: {result.stdout}")
+            if result.stderr:
+                self.logger.error(f"Error en git pull: {result.stderr}")
+        except Exception as e:
+            self.logger.error(f"Error al realizar git pull: {e}")
+
+    def run(self):
+        """
+        Ejecuta el proceso de actualización del repositorio.
+        """
+        self.logger.debug("Iniciando el proceso de git pull")
+        self.git_pull()
+        
+if __name__ == "__main__":
+    repo_path = 'https://github.com/AgustinMadygraf/analizadorDeProyecto/'
+    updater = RepoUpdater(repo_path)
+    updater.run()
